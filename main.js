4d8944ab43f305199848ca828ca07b7cfc438bf8
gael-src <src.gael@gmail.com>
2021-01-27 08:38:06 +0100

    created txt and json test files

--- main.js
+++ main.js
@@ -3,12 +3,36 @@ console.log("README.md:");
 const searchQueryURL =
 	// "https://api.github.com/repos/gael-src/github-api";
 	// "https://api.github.com/repos/gael-src/github-api/contents/README.md";
-	"https://api.github.com/repos/gael-src/github-api/contents/title.txt";
-// "https://raw.githubusercontent.com/gael-src/github-api/main/title.txt";
+	// "https://raw.githubusercontent.com/gael-src/github-api/main/README.md";
+	// "https://api.github.com/repos/gael-src/github-api/contents/title.txt";
 
-window.addEventListener("DOMContentLoaded", function (e) {
-	return fetch(searchQueryURL)
-		.then((result) => result.json())
-		.then((response) => console.log(response))
-		.catch((err) => console.log(err));
-});
+	window.addEventListener("DOMContentLoaded", function (e) {
+		fetchAsync();
+		return fetch(searchQueryURL)
+			.then((result) => result.json())
+			.then((response) => {
+				console.log("response"), console.log(response);
+			})
+			.catch((err) => console.log(err));
+	});
+
+// ASYNC AWAIT FETCH
+async function fetchAsync() {
+	const requestOptions = {
+		method: "GET",
+		redirect: "follow",
+	};
+
+	await fetch(
+		// "https://api.github.com/repos/gael-src/github-api",
+		// "https://api.github.com/repos/gael-src/github-api/contents/README.md",
+		"https://raw.githubusercontent.com/gael-src/github-api/main/README.md",
+		// "https://api.github.com/repos/gael-src/github-api/contents/title.txt",
+		requestOptions
+	)
+		.then((response) => response.json())
+		.then((result) => {
+			console.log("result"), console.log(result);
+		})
+		.catch((error) => console.log("error:", error));
+}
