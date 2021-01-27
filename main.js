d65b7bec269b1bbd6df240682b64e6cbdf2b58ef
gael-src <src.gael@gmail.com>
2021-01-27 08:49:53 +0100

    update

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ console.log("README.md:");
 
 const searchQueryURL =
 	// "https://api.github.com/repos/gael-src/github-api";
-	// "https://api.github.com/repos/gael-src/github-api/contents/README.md";
-	// "https://raw.githubusercontent.com/gael-src/github-api/main/README.md";
-	// "https://api.github.com/repos/gael-src/github-api/contents/title.txt";
+	"https://api.github.com/repos/gael-src/github-api/contents/README.md";
+// "https://raw.githubusercontent.com/gael-src/github-api/main/README.md";
+// "https://api.github.com/repos/gael-src/github-api/contents/title.txt";
 
-	window.addEventListener("DOMContentLoaded", function (e) {
-		fetchAsync();
-		return fetch(searchQueryURL)
-			.then((result) => result.json())
-			.then((response) => {
-				console.log("response"), console.log(response);
-			})
-			.catch((err) => console.log(err));
-	});
+window.addEventListener("DOMContentLoaded", function (e) {
+	fetchAsync();
+	return fetch(searchQueryURL)
+		.then((result) => result.json())
+		.then((response) => {
+			console.log("response"), console.log(response);
+		})
+		.catch((err) => console.log(err));
+});
 
 // ASYNC AWAIT FETCH
 async function fetchAsync() {
