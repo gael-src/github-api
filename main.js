5e0f32c409e1548170934faf7124c45181240fd1
gael-src <src.gael@gmail.com>
2021-01-27 09:19:05 +0100

    update

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 console.log("README.md:");
 
-const searchQueryURL =
-	// "https://api.github.com/repos/gael-src/github-api";
-	"https://api.github.com/repos/gael-src/github-api/contents/README.md";
+const searchQueryURL = "https://api.github.com/repos/gael-src/github-api";
+// "https://api.github.com/repos/gael-src/github-api/contents/README.md";
 // "https://raw.githubusercontent.com/gael-src/github-api/main/README.md";
 // "https://api.github.com/repos/gael-src/github-api/contents/title.txt";
 
 window.addEventListener("DOMContentLoaded", function (e) {
 	fetchAsync();
+	txtOutput();
+	jsonOutput();
 	return fetch(searchQueryURL)
 		.then((result) => result.json())
 		.then((response) => {
@@ -24,9 +25,9 @@ async function fetchAsync() {
 	};
 
 	await fetch(
-		// "https://api.github.com/repos/gael-src/github-api",
+		"https://api.github.com/repos/gael-src/github-api",
 		// "https://api.github.com/repos/gael-src/github-api/contents/README.md",
-		"https://raw.githubusercontent.com/gael-src/github-api/main/README.md",
+		// "https://raw.githubusercontent.com/gael-src/github-api/main/README.md",
 		// "https://api.github.com/repos/gael-src/github-api/contents/title.txt",
 		requestOptions
 	)
@@ -35,4 +36,21 @@ async function fetchAsync() {
 			console.log("result"), console.log(result);
 		})
 		.catch((error) => console.log("error:", error));
+}
+
+// TXT
+function txtOutput() {
+	console.log("txtOutput");
+	const title = document.querySelector(".h3--title");
+	const article = document.querySelector(".p--article");
+	title.innerHTML = "Placeholder Title";
+	article.innerHTML = "Placeholder Article";
+}
+
+// JSON
+function jsonOutput() {
+	console.log("jsonOutput");
+	let articles = {};
+	const jsonWrapper = document.querySelector(".json--wrapper");
+	jsonWrapper.innerHTML = "Placeholder JSON";
 }
